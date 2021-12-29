aa2f4d240fa7400ffb68dc0fefa0c2f24635c0f9
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2021-12-29 15:11:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import GithubSearchHooks from "./Component/GithubSearchHooks";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>useContext Hooks</h1>
+      <br />
+      <GithubSearchHooks />
+    </div>
+  );
+}
